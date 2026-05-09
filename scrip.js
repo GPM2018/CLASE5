1920866f158258895d31a6f16f36762e0276acbd
Tu Nombre <presen.gus07@gmail.com>
2026-05-09 11:14:05 -0300

    CLASE5

---
+++ scrip.js
@@ -0,0 +1,20 @@
+ <script>
+        // Ejemplo 1: Mostrar mensaje al copiar texto
+        function mostrarMensajeCopia() {
+            document.getElementById("mensajeCopia").innerHTML = "¡Copiaste el texto!";
+        }
+
+        // Ejemplo 2: Mostrar mensaje al cortar texto
+        function mostrarMensajeCorte() {
+            document.getElementById("mensajeCorte").innerHTML = "¡Cortaste el texto!";
+        }
+
+        // Ejemplo 3: Agrandar y reducir la imagen
+        function agrandarImagen(x) {
+            x.style.transform = "scale(3)";
+        }
+
+        function reducirImagen(x) {
+            x.style.transform = "scale(1)";
+        }
+    </script>
